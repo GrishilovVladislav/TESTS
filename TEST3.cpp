4d1f5244ebbfba0b2b56931f85452c8609b6ef51
GrishilovVladislav <79422175+GrishilovVladislav@users.noreply.github.com>
2021-07-01 09:09:21 +0300

    Add files via upload

---
+++ TEST3.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+#define MAXSIZE 1000
+
+int main()
+{
+    char str[MAXSIZE];
+    int size, max = 0;
+    int i = 0;
+    int counter = 0;
+    int maxBegin = 0;
+    char res[MAXSIZE];
+
+    gets_s(str);
+    size = strlen(str);
+
+    for (i = 0; i < size; i++)
+    {
+        if (str[i] != ' ')
+        {
+            counter++;
+            if (counter > max)
+            {
+                max = counter;
+                maxBegin = i - max + 1;
+            }
+        }
+        else
+        {
+            counter = 0;
+        }
+    }
+    for (i = maxBegin; i < size - max; i++)
+    {
+        str[i] = str[i + max];
+        
+    }
+    puts(str);
+}
+
